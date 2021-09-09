525819898632431c5df564bf054780b154c13e4d
BuildTools <unconfigured@null.spigotmc.org>
2021-09-08 23:34:38 -0700

    fix NoneType error during guild search

--- CPSC103Bot.py
+++ CPSC103Bot.py
@@ -6,8 +6,11 @@ from dotenv import load_dotenv
 intents = discord.Intents.all()
 bot = commands.Bot(command_prefix="!", intents=intents)
 
-guild = discord.utils.get(bot.guilds, id=885385922361827358)
-landing_role = discord.utils.get(guild.roles, id=885387549948911656)
+
+def get_landing_role():
+    guild = discord.utils.get(bot.guilds, id=885385922361827358)
+    landing_role = discord.utils.get(guild.roles, id=885387549948911656)
+    return landing_role
 
 
 @bot.event
@@ -19,12 +22,13 @@ async def on_ready():
 async def on_raw_reaction_add(payload):
     if payload.channel_id == 885386228625711115:
         if payload.emoji.name == "✅":
-            await payload.member.remove_roles(landing_role)
+            await payload.member.remove_roles(get_landing_role())
 
 
 @bot.event
 async def on_member_join(member):
-    await member.add_roles(landing_role)
+    await member.add_roles(get_landing_role())
+
 
 # shout out to https://github.com/Person314159/cs221bot for how to hide the bot token
 load_dotenv()
